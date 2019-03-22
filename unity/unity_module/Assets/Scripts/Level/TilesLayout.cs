dc1e5781083399105ba83b272abbc99574f492fa
Bogomil <kbogomil@gmail.com>
2019-03-22 22:43:15 +0200

    Changed: adjusting orthographic size relative to the screen size.
    Changed: background covers more area
    Changed: extracted prefabs

---
+++ unity/unity_module/Assets/Scripts/Level/TilesLayout.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilesLayout : MonoBehaviour
+{
+    public int tilesPerRow = 0;
+
+    public int tilesPerColumn = 0;
+
+    public float paddingLeft = 0;
+
+    public float paddingRight = 0;
+
+    public GameObject tile;
+
+    private List<List<GameObject>> grid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var screenAspect = (float) Screen.width / Screen.height;
+        var camWidth = screenAspect * 2f * Camera.main.orthographicSize;
+
+        var desiredWidth = tilesPerRow + paddingLeft + paddingRight;
+        Camera.main.orthographicSize = desiredWidth / (screenAspect * 2f);
+
+        var newTile = (GameObject)Instantiate(tile, transform);
+
+        //Debug.Log(newTile);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AddGameObject(GameObject game)
+    {
+
+    }
+}
