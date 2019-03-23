99592f299c1aa043ef22c586acc17c04256c5ddc
Bogomil <kbogomil@gmail.com>
2019-03-23 17:06:29 +0200

    Changed: Adding a hard-coded grid of tiles

--- unity/unity_module/Assets/Scripts/Level/LevelLoader.cs
+++ unity/unity_module/Assets/Scripts/Level/LevelLoader.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class LevelLoader : MonoBehaviour
 {
+    public GameObject TileGameObject;
+    public GameObject ObstacleGameObject;
+
+    public int TilesPerRow;
+    public int TilesPerColumn;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        var content = new TilesLayout.TilesLayoutContent(TilesPerColumn, TilesPerRow);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for (int col = 0; col < TilesPerColumn; col++)
+        {
+            for (int row = 0; row < TilesPerRow; row++)
+            {
+                var cell = new TilesLayout.TilesLayoutCell(TileGameObject);
+                content.SetCell(cell, row, col);
+            }
+        }
+
+        content.Measure();
+
+        var tilesLayoutObject = GameObject.Find("TilesLayout");
+        var tilesLayout = tilesLayoutObject.GetComponent(typeof(TilesLayout.TilesLayout)) as TilesLayout.TilesLayout;
+
+        tilesLayout.Content = content;
     }
 }
---
+++ unity/unity_module/Assets/Scripts/Level/TilesLayoutCell.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TilesLayout
+{
+    public class TilesLayoutCell
+    {
+        public GameObject GameObject { get; set; }
+
+        public float MarginLeft { get; set; }
+        public float MarginTop { get; set; }
+        public float MarginRight { get; set; }
+        public float MarginBottom { get; set; }
+
+        public TilesLayoutCell(GameObject gameObject)
+        {
+            GameObject = gameObject;
+            // TODO - make a TilesLayoutCell prefab and set those from the Unity editor.
+            MarginLeft = 0.1f;
+            MarginRight = 0.1f;
+            MarginTop = 0.1f;
+            MarginBottom = 0.1f;
+        }
+
+        public float MeasuredWidth
+        {
+            get
+            {
+                float GameObjectWidth;
+                if (GameObject == null)
+                {
+                    GameObjectWidth = 1;
+                }
+                else
+                {
+                    GameObjectWidth = GameObject.transform.localScale.x;
+                }
+
+                return GameObjectWidth + MarginLeft + MarginRight;
+            }
+        }
+
+        public float MeasuredHeight
+        {
+            get
+            {
+                float GameObjectHeight;
+                if (GameObject == null)
+                {
+                    GameObjectHeight = 1;
+                } else
+                {
+                    GameObjectHeight = GameObject.transform.localScale.y;
+                }
+
+                return GameObjectHeight + MarginTop + MarginBottom;
+            }
+        }
+    }
+}
