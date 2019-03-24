539061a7ce0c032f0f43b20c3e7383234688a5bd
Bogomil <kbogomil@gmail.com>
2019-03-24 19:11:38 +0200

    Added: Class for binding text to tiles and listening for click events

---
+++ unity/unity_module/Assets/Scripts/Level/LetterTileBinding.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LetterTile
+{
+    public class LetterTileBinding : MonoBehaviour
+    {
+        private Text TextScriptComponent;
+
+        public delegate void TileOnClickListener(string letter);
+        public event TileOnClickListener OnTileClick;
+
+        public string Letter
+        {
+            get
+            {
+                AssertInitialized();
+                return TextScriptComponent.text;
+            }
+            set
+            {
+                AssertInitialized();
+                if (!string.IsNullOrEmpty(value) && value.Length != 1)
+                {
+                    throw new InvalidOperationException("The value must be with length 1");
+                }
+                TextScriptComponent.text = value;
+            }
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            var textGameObject = GameObject.Find($"{name}/Canvas/Text");
+            TextScriptComponent = textGameObject.GetComponent(typeof(Text)) as Text;
+        }
+
+        void OnMouseDown()
+        {
+            Debug.Log($"\"{Letter}\" clicked!");
+            OnTileClick?.Invoke(Letter);
+        }
+
+        private void AssertInitialized()
+        {
+            if (TextScriptComponent == null)
+            {
+                throw new InvalidOperationException("Script not yet initialized.");
+            }
+        }
+    }
+
+}
