e572a6b872c5714f637f19c43edb56ca1dceddef
Bogomil <kbogomil@gmail.com>
2019-03-30 23:13:59 +0200

    Changed: Loading different shuffled letters into tiles.
    Added: repository for level data
    Changed: implemented tile selection logic

---
+++ unity/unity_module/Assets/Scripts/Level/Repository/LevelData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Level.Repository
+{
+    public class LevelData
+    {
+
+        public List<string> HintWords { get; }
+
+        public LevelData(List<string> hintWords)
+        {
+            HintWords = hintWords;
+        }
+    }
+}
--- unity/unity_module/Assets/Scripts/Prefabs/LetterTileBinding.cs
+++ unity/unity_module/Assets/Scripts/Prefabs/LetterTileBinding.cs
@@ -13,21 +13,48 @@ namespace LetterTile
         public delegate void TileOnClickListener(string letter);
         public event TileOnClickListener OnTileClick;
 
+        private Field PendingBindingFields = Field.NONE;
+
+        private string _Letter;
+        private bool _Selected = false;
+
         public string Letter
         {
             get
             {
-                AssertInitialized();
-                return TextScriptComponent.text;
+                return _Letter;
             }
             set
             {
-                AssertInitialized();
                 if (!string.IsNullOrEmpty(value) && value.Length != 1)
                 {
                     throw new InvalidOperationException("The value must be with length 1");
                 }
-                TextScriptComponent.text = value;
+
+                if (_Letter != value) {
+                    PendingBindingFields |= Field.LETTER;
+                }
+
+                // Probably not the best place to convert it to upper case but 
+                // there is no reason to over-complicate the code. I set it here as I 
+                // want the view to decide whether to show upper or lower case letters.
+                _Letter = value.ToUpper();
+            }
+        }
+
+        public bool Selected
+        {
+            get
+            {
+                return _Selected;
+            }
+            set
+            {
+                if (_Selected != value)
+                {
+                    PendingBindingFields |= Field.SELECTED;
+                }
+                _Selected = value;
             }
         }
 
@@ -38,17 +65,36 @@ namespace LetterTile
             TextScriptComponent = textGameObject.GetComponent(typeof(Text)) as Text;
         }
 
+        void Update()
+        {
+            if ((PendingBindingFields & Field.LETTER) == Field.LETTER)
+            {
+                if (TextScriptComponent != null)
+                {
+                    TextScriptComponent.text = _Letter;
+                    PendingBindingFields &= ~Field.LETTER;
+                }
+            }
+
+            if ((PendingBindingFields & Field.SELECTED) == Field.SELECTED)
+            {
+                // TODO - Update the game object when we support selection.
+                PendingBindingFields &= ~Field.SELECTED;
+            }
+        }
+
         void OnMouseDown()
         {
+            Selected = !Selected;
             OnTileClick?.Invoke(Letter);
         }
 
-        private void AssertInitialized()
+        [Flags]
+        private enum Field
         {
-            if (TextScriptComponent == null)
-            {
-                throw new InvalidOperationException("Script not yet initialized.");
-            }
+            NONE = 0,
+            LETTER = 1,
+            SELECTED = 2
         }
     }
 
