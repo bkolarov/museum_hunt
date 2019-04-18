c82093b7fda914c0d5ca52d896e9ac1a836f1ffe
Bogomil <kbogomil@gmail.com>
2019-04-18 23:19:56 +0300

    Changed: Added movement to the player

--- unity/unity_module/Assets/Scripts/Prefabs/Player/MovementController.cs
+++ unity/unity_module/Assets/Scripts/Prefabs/Player/MovementController.cs
@@ -1,15 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
+    public float SpeedX = 5.0f;
 
-    
+    private float raycastDistance = 0.0f;
+    private ConstantForce2D constantForce2D;
 
-    void OnCollisionEnter2D(Collision2D collision)
+    void Start()
     {
-        
-       
+
+        raycastDistance = transform.localScale.x * GetComponent<BoxCollider2D>().size.x;
+
+        constantForce2D = GetComponent<ConstantForce2D>();
+        constantForce2D.relativeForce = new Vector2(SpeedX, 0.0f);
+
+    }
+
+    void FixedUpdate()
+    {
+        RaycastHit2D hitRight = Physics2D.Raycast(GetComponent<Renderer>().bounds.center, Vector2.right, raycastDistance);
+        if (hitRight.collider != null)
+        {
+        	constantForce2D.relativeForce = new Vector2(-SpeedX, 0.0f);
+        }
+
+        RaycastHit2D hitLeft = Physics2D.Raycast(GetComponent<Renderer>().bounds.center, Vector2.left, raycastDistance);
+        if (hitLeft.collider != null)
+        {
+        	constantForce2D.relativeForce = new Vector2(SpeedX, 0.0f);
+        }
     }
 }
