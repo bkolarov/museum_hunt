94474286e081d8031b68a1e47a7ed03db698d7ba
Bogomil <kbogomil@gmail.com>
2019-04-27 15:03:14 +0300

    Changed: Added application class and gradle dependencies

--- android/MuseumHunt/app/src/main/java/bg/tusofia/pmu/museumhunt/application/MuseumHuntApplication.kt
+++ android/MuseumHunt/app/src/main/java/bg/tusofia/pmu/museumhunt/application/MuseumHuntApplication.kt
@@ -1,2 +1,12 @@
 package bg.tusofia.pmu.museumhunt.application
 
+import android.app.Application
+
+class MuseumHuntApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+}
